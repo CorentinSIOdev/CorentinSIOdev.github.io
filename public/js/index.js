ace55fb6af955fbbb7ebaf01719bc3468cff65b7
Corentin Bonifacio <78152277+CorentinSIOdev@users.noreply.github.com>
2024-02-03 17:38:52 +0100

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -3,29 +3,71 @@ document.addEventListener('DOMContentLoaded', () => {
     var hamburger = document.querySelector('.nav_hamburger');
     var navButtons = document.querySelector('.nav_buttons');
     
-    // Toggle navButtons when hamburger is clicked
+    // Action when user clicks hamburger
     hamburger.addEventListener('click', () => {
-        navButtons.classList.toggle('show');
+        if (navButtons.classList.contains('is-open')) {
+            // Close animation
+            navButtons.classList.add('close-animation');
+            navButtons.classList.remove('open-animation');
+
+            // Remove open classes after animation ends
+            navButtons.addEventListener('animationend', () => {
+                navButtons.classList.remove('is-open', 'close-animation');
+            }, { once: true });
+        } else {
+            // Open animation
+            navButtons.classList.add('is-open', 'open-animation');
+        }
     });
 
     // Close navButtons when user scrolls or clicks outside of navButtons
     window.addEventListener('scroll', () => {
         hamburger.checked = false;
-        navButtons.classList.remove('show');
+
+        if(navButtons.classList.contains('is-open')) {
+            // Close animation
+            navButtons.classList.add('close-animation');
+            navButtons.classList.remove('open-animation');
+
+            // Remove open classes after animation ends
+            navButtons.addEventListener('animationend', () => {
+                navButtons.classList.remove('is-open', 'close-animation');
+            }, { once: true });
+        }
     })
 
     // Close navButtons when user clicks outside of navButtons
     window.addEventListener('click', (e) => {
         if (e.target != hamburger && e.target != navButtons) {
             hamburger.checked = false;
-            navButtons.classList.remove('show');
+
+            if(navButtons.classList.contains('is-open')) {
+                // Close animation
+                navButtons.classList.add('close-animation');
+                navButtons.classList.remove('open-animation');
+
+                // Remove open classes after animation ends
+                navButtons.addEventListener('animationend', () => {
+                    navButtons.classList.remove('is-open', 'close-animation');
+                }, { once: true });
+            }
         }
     });
 
     // Close navButtons when user resizes window
     window.addEventListener('resize', () => {
         hamburger.checked = false;
-        navButtons.classList.remove('show');
+
+        if(navButtons.classList.contains('is-open')) {
+            // Close animation
+            navButtons.classList.add('close-animation');
+            navButtons.classList.remove('open-animation');
+
+            // Remove open classes after animation ends
+            navButtons.addEventListener('animationend', () => {
+                navButtons.classList.remove('is-open', 'close-animation');
+            }, { once: true });
+        }
     });
 });
 /* -*-*- END NAVIGATION BAR -*-*- */
